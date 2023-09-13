bf39ab31aaf35dba1d34a267eb410d4285f80545
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-14 01:24:10 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const taskDescription = document.getElementById("new-task-description").value;
+      addTask(taskDescription);
+      form.reset();
+  });
+
+  function addTask(description) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = description;
+      taskList.appendChild(taskItem);
+  }
 });
+
